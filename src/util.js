e8290040882b1e9f53887d63e1092c0fcd3414d8
xing.he <1245036094@qq.com>
2020-10-02 18:31:09 +0800

    feat: fix node permission denied

--- src/util.js
+++ src/util.js
@@ -3,6 +3,11 @@ const fs = require('fs-extra');
 const download = require('download-git-repo')
 const ora = require('ora')
 const logSymbols = require("log-symbols");
+const path = require('path');
+const ini = require('ini');
+const initChoices = require('../src/choices.json')
+
+const SYRC = path.join(process.env.HOME, '.syrc');
 
 const makeDir = (projectRoot)=>{
   if("." !== projectRoot){
@@ -13,7 +18,7 @@ const makeDir = (projectRoot)=>{
 const clone = (target, url)=>{
   const spinner = ora({
     text:`download...`,
-    color:'yellow',
+    color:'green',
   });
   spinner.start()
   return new Promise((resolve,reject) => {
@@ -32,6 +37,39 @@ const clone = (target, url)=>{
   })
 }
 
+
+const checkChoices = ()=>{
+  const choices = getChoices;
+  if(choices.length === 0){
+    console.log(chalk.red(`There is no choice exist, Add One please!`));
+    console.log(chalk.green(`run: sy add
+`))
+    return process.exit(-1);
+  }
+}
+
+const getINIInfo = (path) =>{
+  return fs.existsSync(path) ? ini.parse(fs.readFileSync(path, 'utf-8')) : null;
+}
+
+const getChoices = ()=>{
+  const choices = getINIInfo(SYRC);
+  if(choices) {
+    return Object.values(choices); 
+  }
+
+  setChoices(initChoices);
+  return initChoices;
+}
+
+const setChoices = (choices)=>{
+  fs.writeFileSync(SYRC, ini.stringify(choices))
+}
+
+
 exports.makeDir = makeDir;
 exports.clone = clone;
+exports.getChoices = getChoices;
+exports.setChoices = setChoices;
+exports.checkChoices = checkChoices;
 
